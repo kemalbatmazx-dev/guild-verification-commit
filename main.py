c8056c1a55af18b2af6064b468c4a5d6db35d345
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:52:49 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:52:39.269Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T11:52:50.519Z
+Commit: Fix broken links in documentation
