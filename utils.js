31d2e1395c3bbc4be4560a13613d6b64dbf476c7
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:51:04 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:50:55.291Z
-Commit: Add dark mode support
+Timestamp: 2026-03-11T11:51:05.585Z
+Commit: Resolve null pointer exception in user service
