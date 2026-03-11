ed202dff45d3c2748f621e9494aecdea34f0ddc5
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:53:02 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:52:52.497Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T11:53:03.091Z
+Commit: Fix CSS styling bug on mobile devices
