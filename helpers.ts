cd6097548e25628b9dba76931b25b233c8b4e621
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:53:19 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:53:00.819Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T11:53:20.268Z
+Commit: Resolve null pointer exception in user service
