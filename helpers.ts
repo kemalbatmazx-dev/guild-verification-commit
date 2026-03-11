b2ee64f7ec8df75c4a684945db582a755d94b163
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:49:02 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:49:01.767Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-11T11:49:03.012Z
+Commit: Fix memory leak in event handlers
