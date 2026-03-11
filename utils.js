c1d7c705469f307ba4a657383c230bc75fb54ef1
kemalbatmazx-dev <kemalbatmazx@gmail.com>
2026-03-11 14:53:13 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T11:53:07.538Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T11:53:13.476Z
+Commit: Fix memory leak in event handlers
